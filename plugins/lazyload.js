274836913f1c455f29aa6bd966ae07ac2027216e
charon616 <milkyway.k16@gmail.com>
2020-01-12 16:26:45 +0900

    load

--- plugins/lazyload.js
+++ plugins/lazyload.js
@@ -4,8 +4,8 @@ import VueLazyload from 'vue-lazyload';
 Vue.use(VueLazyload, {
   preLoad: 1.1,
   attempt: 1,
-  error: 'error.png',
-  loading: 'loading.gif',
+  error: '~/assets/error.png',
+  loading: '~/assets/loading.gif',
   observer: true,
 
   observerOptions: {
