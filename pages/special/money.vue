ccab7707cc54ef55e4c7efc7cb207570cf7bdab6
charon616 <milkyway.k16@gmail.com>
2020-08-18 17:22:39 +0900

    add gcls

---
+++ pages/special/money.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+.container
+  #page-wrap
+    .main-content#special
+      h2.page-title SPECIAL
+      svg#svg-bg(xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1500 50" preserveAspectRatio="none")
+        path(d="M0,0 v25 q10,10 20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 t20,0 v-25 Z" fill="#3eba90")
+      
+
+
+</template>
+<script>
+
+export default {
+    layout: 'wide',
+    components: {
+    },
+    data() {
+        return{
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style scoped lang="stylus">
+.container
+    width 100%
+    background white
+    border 10px solid black
+
+</style>
